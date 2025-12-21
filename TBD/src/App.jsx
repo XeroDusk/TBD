2de07e428fde61ff018bc55c4b3a88078a57bc83
dusk <asutoshpradhan424@gmail.com>
2025-12-22 03:52:55 +0530

    File Structure

--- TBD/src/App.jsx
+++ TBD/src/App.jsx
@@ -1,11 +1,27 @@
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Signup from './components/Signup/Signup'
+import Contact from './components/Contact/Contact'
+import About from './components/About/About'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<App />}>
+        <Route path='' element={<Home />}/>
+        <Route path='login' element={<Login />}/>
+        <Route path='signup' element={<Signup />}/>
+        <Route path='contact' element={<Contact />}/>
+        <Route path='about' element={<About />}/>
 
-  return (
-    <>
-      <h1>TBD</h1>
-    </>
+      </Route>
+    )
   )
+
+  return <RouterProvider router={router} />
 }
+
 
 export default App
